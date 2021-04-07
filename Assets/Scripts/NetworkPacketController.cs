e1b89dbded30310837ae7a47fa017c279e582391
raphaexl <ebatchas@1337.ma>
2021-04-07 11:05:57 +0100

    Unet seems not available on mac

---
+++ Assets/Scripts/NetworkPacketController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkPacketController : NetworkBehaviour
+{
+    [System.Serializable]
+    public class Package
+    {
+        public float Horizontal;
+        public float Vertical;
+        public float Timestamp;
+    }
+
+    [System.Serializable]
+    public class ReceivedPackage
+    {
+        public float X;
+        public float Y;
+        public float Z;
+        public float Timestamp;
+    }
+
+    public NetworkPacketManager<Package> m_packageManager;
+    public NetworkPacketManager<Package> PackageManager
+    {
+        get
+        {
+            if (m_packageManager == null)
+            {
+                m_packageManager = new NetworkPacketManager<Package>();
+                if (isLocalPlayer)
+                {
+                    m_packageManager.OnRequiredPackageTransmit += TransmitPackageToServer;
+                }
+            }
+            return m_packageManager;
+        }
+    }
+
+    public NetworkPacketManager<ReceivedPackage> m_serverPackageManager;
+    public NetworkPacketManager<ReceivedPackage> ServerPackageManager
+    {
+        get
+        {
+            if (m_serverPackageManager == null)
+            {
+                m_serverPackageManager = new NetworkPacketManager<ReceivedPackage>();
+                if (isServer)
+                {
+                    m_serverPackageManager.OnRequiredPackageTransmit += TransmitPackageToClients;
+                }
+            }
+            return m_serverPackageManager;
+        }
+    }
+
+    private void TransmitPackageToServer(byte[] bytes)
+    {
+        CmdTransmitPackages(bytes);
+    }
+
+    private void TransmitPackageToClients(byte[] bytes)
+    {
+        RpcReceivedDataOnClient(bytes);
+    }
+
+    [Command] 
+    private void CmdTransmitPackages(byte[] bytes)
+    {
+        PackageManager.ReceivePackages(bytes);
+    }
+
+    [ClientRpc]
+    private void RpcReceivedDataOnClient(byte[] data)
+    {
+        ServerPackageManager.ReceivePackages(data);
+    }
+
+    public virtual void FixedUpdate()
+    {
+        PackageManager.Tick();
+        ServerPackageManager.Tick();
+    }
+}
---
+++ Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof (CharacterController))]
+public class PlayerNetwork : NetworkPacketController
+{
+    [SerializeField]
+    private float moveSpeed = .4f;
+    [SerializeField]
+    [Range(0.1f, 1.0f)]
+    private float networkSendRate = 0.5f;
+    [SerializeField]
+    private bool isPredictionEnabled = default ;
+    [SerializeField]
+    private float correctionThreshold = 0.01f;
+    
+    CharacterController controller;
+    List<ReceivedPackage> predictedPackages;
+    Vector3 lastPosition;
+
+    private float nextCorrectTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        PackageManager.SendSpeed = networkSendRate;
+        ServerPackageManager.SendSpeed = networkSendRate;
+        predictedPackages = new List<ReceivedPackage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LocalPlayerUpdate();
+        ServerPlayerUpdate();
+        RemoteClientUpdate();
+    } 
+
+    void LocalPlayerUpdate()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        if (!isLocalPlayer)
+            return;
+        if (inputX != 0 || inputY != 0)
+        {
+            float timestamp = Time.time;
+            PackageManager.AddPackage(
+                new Package
+                {
+                    Horizontal = inputX,
+                    Vertical = inputY,
+                    Timestamp = timestamp
+                });
+            if (isPredictionEnabled)
+            {
+                Move(inputX * moveSpeed, inputY * moveSpeed);
+                predictedPackages.Add(new ReceivedPackage
+                {
+                    X = transform.position.x,
+                    Y = transform.position.y,
+                    Z = transform.position.z,
+                    Timestamp = timestamp
+                });
+            }
+        } 
+      
+    }
+
+    void ServerPlayerUpdate()
+    {
+        if (!isServer || isLocalPlayer)
+            return;
+        Package packageData = PackageManager.GetNextDataReceived();
+        if (packageData == null) { return; }
+        Move(packageData.Horizontal * moveSpeed, packageData.Vertical * moveSpeed);
+        if (transform.position == lastPosition)
+            return;
+        lastPosition = transform.position;
+
+        ServerPackageManager.AddPackage(new ReceivedPackage
+        {
+            X = transform.position.x,
+            Y = transform.position.y,
+            Z = transform.position.z,
+            Timestamp = packageData.Timestamp
+        }); 
+    }
+
+    void    Move(float horizontal, float vertical)
+    {
+        controller.Move(new Vector3(horizontal, 0, vertical));
+    }
+
+    private void RemoteClientUpdate()
+    {
+        if (isServer) { return; }
+        ReceivedPackage data = ServerPackageManager.GetNextDataReceived();
+        if (data == null)
+            return;
+        if (isLocalPlayer && isPredictionEnabled)
+        {
+            var transmittedPackage = predictedPackages.Where(x => x.Timestamp == data.Timestamp).FirstOrDefault();
+            if ( transmittedPackage == null)
+            {
+                //You should do something here :-) 
+                return;
+            }
+            if (Vector3.Distance(new Vector3(transmittedPackage.X, transmittedPackage.Y, transmittedPackage.Z),
+                new Vector3(data.X, data.Y, data.Z)) > correctionThreshold)
+            {
+                transform.position = new Vector3(data.X, data.Y, data.Z);
+            }
+            //Clear all pedicted
+            predictedPackages.RemoveAll(x => x.Timestamp <= data.Timestamp); 
+        }
+        else { transform.position = new Vector3(data.X, data.Y, data.Z); }
+        
+    }
+}
